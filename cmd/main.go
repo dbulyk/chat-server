a69f697ffe8cf4ddf6f13d664957a2d5314fd815
bulykind <bulykind@terralink-global.com>
2024-11-12 17:14:54 +0300

    добавил считываение конфигов, убрал деплой сборок для ускорения выполнения заданий

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"chat_server/internal/config"
+	"chat_server/internal/config/env"
 	desc "chat_server/pkg/chat_server_v1"
 	"context"
-	"fmt"
+	"flag"
 	"log"
 	"net"
 	"time"
@@ -16,22 +18,41 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-const (
-	grpcPort = 50051
-	dbDSN    = "host=localhost port=5432 dbname=postgres user=admin password=admin sslmode=disable"
-)
+var configPath string
 
 type server struct {
 	desc.UnimplementedChatServerV1Server
 	db *pgx.Conn
 }
 
-// main запускает сервер на указанном порту
+// init записывает параметр конфига
+func init() {
+	flag.StringVar(&configPath, "config-path", ".env", "path to config file")
+}
+
+// main запускает сервер на указанном в конфиге порту
 func main() {
+	flag.Parse()
+
+	err := config.Load(configPath)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	grpcConfig, err := env.NewGRPCConfig()
+	if err != nil {
+		log.Fatalf("failed to get grpc config: %v", err)
+	}
+
+	pgConfig, err := env.NewPGConfig()
+	if err != nil {
+		log.Fatalf("failed to get pg config: %v", err)
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 
-	conn, err := pgx.Connect(ctx, dbDSN)
+	conn, err := pgx.Connect(ctx, pgConfig.DSN())
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
@@ -43,7 +64,7 @@ func main() {
 		}
 	}(conn, ctx)
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	lis, err := net.Listen("tcp", grpcConfig.Address())
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
